1c5eaee5d41734d2bfd12a58abcba27a9dd7d2eb
Rafael Oliveira <rafael.oliveira@spot-edu.com.br>
2020-07-12 00:24:17 -0300

    Add logic to create and get all transactions

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,23 +6,72 @@ interface Balance {
   total: number;
 }
 
+interface ReturnBalanceTransactions {
+  transactions: Transaction[];
+  balance: Balance;
+}
+
+interface ObjectTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
+  private balance: Balance;
+
   constructor() {
     this.transactions = [];
+    this.balance = {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    };
   }
 
-  public all(): Transaction[] {
-    // TODO
+  public all(): ReturnBalanceTransactions {
+    return {
+      transactions: this.transactions,
+      balance: this.getBalance(),
+    };
   }
 
   public getBalance(): Balance {
-    // TODO
+    const incomeTotal = this.transactions
+      .filter(transaction => transaction.type === 'income')
+      .map(transaction => {
+        return transaction.value;
+      })
+      .reduce((total, values): number => {
+        return (total += values);
+      }, 0);
+
+    const outcomeTotal = this.transactions
+      .filter(transaction => transaction.type === 'outcome')
+      .map(transaction => {
+        return transaction.value;
+      })
+      .reduce((total, values): number => {
+        return (total += values);
+      }, 0);
+
+    const total = incomeTotal - outcomeTotal;
+
+    this.balance = {
+      income: incomeTotal,
+      outcome: outcomeTotal,
+      total,
+    };
+
+    return this.balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: ObjectTransaction): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
